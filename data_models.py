19d9c641847d5aee42c9cfcdb70ad0d0a11bc4e9
pator-pixel <pator@gmx.net>
2026-06-01 12:29:04 +0200

    Baumstruktur hinzugefügt und erster Test

---
+++ data_models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Author(db.Model):
+    __tablename__ = "authors"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(200), nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(20), nullable=False, unique=True)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+
+    author_id = db.Column(
+        db.Integer,
+        db.ForeignKey("authors.id"),
+        nullable=False
+    )
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
